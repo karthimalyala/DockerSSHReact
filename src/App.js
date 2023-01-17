edfa2f166818fefb8c71eb875aee040a768b22fd
karthi2210 <karthitejam@gmail.com>
2023-01-17 16:59:32 +0530

    appSettingRepro

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const DFM_URL = process.env.REACT_APP_DFM_Link
+  const REACT_URL = process.env.REACT_APP_ICM_Link
+
+  useEffect(() => {
+    console.log("usefeffect")
+    console.log("React URL", REACT_URL)
+    console.log("DFM", DFM_URL)
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
         </p>
         <a
           className="App-link"
